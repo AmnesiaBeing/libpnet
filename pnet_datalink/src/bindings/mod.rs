02fa6241a7c9169f93e6eda620f3a948c82434c5
AmnesiaBeing <727184139@qq.com>
2024-02-11 00:07:49 +0800

    解绑bingdings和windows

--- pnet_datalink/src/bindings/mod.rs
+++ pnet_datalink/src/bindings/mod.rs
@@ -6,20 +6,23 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#[cfg(any(
-    target_os = "freebsd",
-    target_os = "openbsd",
-    target_os = "netbsd",
-    target_os = "illumos",
-    target_os = "solaris",
-    target_os = "macos",
-    target_os = "ios",
-    windows
+#[cfg(all(
+    feature = "bpf",
+    any(
+        target_os = "freebsd",
+        target_os = "openbsd",
+        target_os = "netbsd",
+        target_os = "illumos",
+        target_os = "solaris",
+        target_os = "macos",
+        target_os = "ios",
+        target_os = "windows"
+    )
 ))]
 pub mod bpf;
 
-#[cfg(any(target_os = "linux", target_os = "android"))]
+#[cfg(all(feature = "linux_dll", any(target_os = "linux", target_os = "android")))]
 pub mod linux;
 
-#[cfg(windows)]
+#[cfg(all(feature = "winpcap", target_os = "windows"))]
 pub mod winpcap;
